157074fce34b4d6837a976544216a730ca8d185f
Shay <33183299+CohenS@users.noreply.github.com>
2023-11-05 22:00:59 +0200

    Update Types.sql

--- concordance/Stored Procedures/Types.sql
+++ concordance/Stored Procedures/Types.sql
@@ -1,47 +1,61 @@
+-- Create a table type to store chapter statistics.
 CREATE TYPE [ChapterStatistics] AS TABLE
 (
-    ChapterNumber INT,
-	CharacterCount INT
+  -- The chapter number.
+  ChapterNumber INT,
+  -- The character count of the chapter.
+  CharacterCount INT
 );
-
 GO;
 
+-- Create a table type to store paragraph statistics.
 CREATE TYPE ParagraphStatistics AS TABLE
 (
-    ParagraphNumber INT,
-	CharacterCount INT
+  -- The paragraph number.
+  ParagraphNumber INT,
+  -- The character count of the paragraph.
+  CharacterCount INT
 );
-
 GO;
 
+-- Create a table type to store line statistics.
 CREATE TYPE LineStatistics AS TABLE
 (
-    LineNumber INT,
-	CharacterCount INT
+  -- The line number.
+  LineNumber INT,
+  -- The character count of the line.
+  CharacterCount INT
 );
-
 GO;
 
+-- Create a table type to store words.
 CREATE TYPE [Words] AS TABLE
 (
-    WordValue VARCHAR(MAX),
-	Paragraph INT,
-	Chapter INT,
-	Line INT,
-	WordNumber INT
+  -- The word value.
+  WordValue VARCHAR(MAX),
+  -- The paragraph number that the word belongs to.
+  Paragraph INT,
+  -- The chapter number that the word belongs to.
+  Chapter INT,
+  -- The line number that the word belongs to.
+  Line INT,
+  -- The word number within the line.
+  WordNumber INT
 );
 GO;
 
-
+-- Create a table type to store phrases.
 CREATE TYPE [Phrase] AS TABLE
 (
-    Word VARCHAR(MAX)
+  -- The word value of the phrase.
+  Word VARCHAR(MAX)
 );
 GO;
 
+-- Create a table type to store word group words.
 CREATE TYPE [WordGroupWords] AS TABLE
 (
-    Word VARCHAR(MAX)
+  -- The word value of the word group word.
+  Word VARCHAR(MAX)
 );
 GO;
-
