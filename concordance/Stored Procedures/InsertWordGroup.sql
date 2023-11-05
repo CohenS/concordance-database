07d89bf62de48d38b4fe8fadfda74cde9f989855
Shay <33183299+CohenS@users.noreply.github.com>
2023-11-05 21:15:12 +0200

    Update InsertWordGroup.sql

--- concordance/Stored Procedures/InsertWordGroup.sql
+++ concordance/Stored Procedures/InsertWordGroup.sql
@@ -1,27 +1,35 @@
+
+
+SQL
+-- Insert a new word group into the WordGroup table.
 CREATE PROCEDURE [dbo].[InsertWordGroup](
-   @WordGroupName VARCHAR(MAX),
-   @Words [WordGroupWords] READONLY,
-   @BookID INT
+  -- The name of the word group.
+  @WordGroupName VARCHAR(MAX),
+  -- A table-valued parameter that contains the word group words.
+  @Words [WordGroupWords] READONLY,
+  -- The ID of the book that the word group belongs to.
+  @BookID INT
 )
 
 AS
 BEGIN
 
-    INSERT INTO [WordGroup]
-               (
-               WordGroupName,
-               WordID,
-               BookID
-               )
-         SELECT 
-                @WordGroupName,
-                W.WordID,
-                @BookID
-    FROM Word W
-    INNER JOIN @Words AS WGW
-        ON
-	        LEN([WGW].[Word]) = [W].[Length] AND
-	        [WGW].[Word] = [W].[Value]
+  -- Insert the word group into the WordGroup table.
+  INSERT INTO [WordGroup]
+    (
+      WordGroupName,
+      WordID,
+      BookID
+    )
+  SELECT
+    @WordGroupName,
+    W.WordID,
+    @BookID
+  FROM Word W
+  INNER JOIN @Words AS WGW
+    ON
+      LEN([WGW].[Word]) = [W].[Length] AND
+      [WGW].[Word] = [W].[Value];
 
 END;
 GO;
